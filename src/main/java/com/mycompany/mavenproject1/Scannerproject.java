50708c6b658852632afbda96d6a5cee6b492016b
aguscarig <aguscar54@gmail.com>
2023-03-09 19:45:23 -0300

    hola

---
+++ src/main/java/com/mycompany/mavenproject1/Scannerproject.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Agus
+ */
+public class Scannerproject {
+
+    public static void main(String[] args) {
+      Scanner console = new Scanner(System.in);
+        
+//            System.out.println("Ingresa dos Numeros");
+//        
+//             System.out.println("Inicial: ");  
+//                int numI = console.nextInt(); 
+//             
+//             System.out.println("Final");
+//                int numF = console.nextInt();
+//              
+//              while (numI <= numF) {
+//                  System.out.println(numI);
+//                  numI = numI +1;   
+//              }
+              
+//             System.out.println("Impresion numeros pares");
+//             
+//            System.out.println("Ingresa dos Numeros");
+//        
+//             System.out.println("Inicial: ");  
+//                int numI = console.nextInt(); 
+//             
+//             System.out.println("Final");
+//                int numF = console.nextInt(); 
+//             
+//             
+//             while (numI <= numF) {
+//                 if (numI%2 == 0) {
+//                     System.out.println(numI);
+//                 } 
+//                 else {
+//                  numI = numI +1;
+//                     System.out.println(numI);
+//                 }
+//                numI = numI + 2;
+//             }
+               System.out.println("Ingresa un Numero mayor a 5");  
+                 
+               for (int i = console.nextInt() ; i > 5 ; i--) {
+                   
+                }
+        }
+                
+    }
+
